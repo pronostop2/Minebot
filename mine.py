825bdf56150d1b2926bd7eefa51d3b8bde0f6ae0
pronostop2 <pronostop23@gmail.com>
2026-06-28 10:05:05 +0100

    Update mine.py

--- mine.py
+++ mine.py
@@ -90,8 +90,9 @@ async def send_signals():
              # ------------ newmsg --------------------
 #         newmsg = "Register using the coupon CASHF and get a 500% bonus on your first deposit and activate the bot. 💎
 # (Minimum $10 required to activate)"
-            await bot.send_message(CHANNEL_ID, "Register using the coupon CASHF and get a 500% bonus on your first deposit and activate the bot. 💎
-(Minimum $10 required to activate)",)      
+            await bot.send_message(
+                CHANNEL_ID, 
+                "Register using the coupon CASHF and get a 500% bonus on your first deposit and activate the bot. 💎 (Minimum $10 required to activate)",)      
             await asyncio.sleep(random.randint(2, 5))
             
         except Exception as e:
